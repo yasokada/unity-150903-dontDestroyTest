046258cee4f513906cd8291cc469c582496652b6
y_okada <yasokada@gmail.com>
2015-09-03 06:47:11 +0900

    add version comment

--- Assets/someProcessScript.cs
+++ Assets/someProcessScript.cs
@@ -3,7 +3,13 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class someProcessScript : MonoBehaviour {
-	
+
+/*
+ * v0.1 20150902
+ *   - Main -> Other -> Mainとシーン遷移した時、someProcessに関連付けたいIF_name, Toggle_testが
+ *   Noneになってしまう。
+ */
+
 	public InputField IF_name;
 	public Toggle Toggle_test;
 	private static bool created = false;
