2061539d3b71e9c86c67b34fc2c614a57b7c00e6
y_okada <yasokada@gmail.com>
2015-09-03 06:44:55 +0900

    has some problem with someProcess GO related with None instead of IF_name, Toggle_test

---
+++ Assets/someProcessScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class someProcessScript : MonoBehaviour {
+	
+	public InputField IF_name;
+	public Toggle Toggle_test;
+	private static bool created = false;
+	
+	void Awake() {
+		if (!created) {
+			created = true;
+			DontDestroyOnLoad (this.gameObject);
+		} else {
+			Destroy (this.gameObject);
+		}
+	}
+	
+	void Start () {
+	}
+	
+	void Update () {
+		
+	}
+}
