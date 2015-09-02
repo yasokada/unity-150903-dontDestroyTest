e973786c4e2fbf385d4b06f99479f9c85c3c7e4e
y_okada <yasokada@gmail.com>
2015-09-03 06:51:20 +0900

    fix > IF_name, Toggle_test == none problem

--- Assets/someProcessScript.cs
+++ Assets/someProcessScript.cs
@@ -5,8 +5,11 @@ using UnityEngine.UI;
 public class someProcessScript : MonoBehaviour {
 
 /*
+ * v0.2 20150903
+ *   - http://qiita.com/7of9/items/78f829bcbda0067c7bcd#comment-251ad36cd28d167d3743
+ *   にて教えていただいた方法を実装した。Noneの問題が解決した。
  * v0.1 20150902
- *   - Main -> Other -> Mainとシーン遷移した時、someProcessに関連付けたいIF_name, Toggle_testが
+ *   - Main -> Other -> Mainとシーン遷移した時、someProcessに関連付けたIF_name, Toggle_testが
  *   Noneになってしまう。
  */
 
@@ -15,11 +18,16 @@ public class someProcessScript : MonoBehaviour {
 	private static bool created = false;
 	
 	void Awake() {
-		if (!created) {
+		if (!created)
+		{
 			created = true;
-			DontDestroyOnLoad (this.gameObject);
-		} else {
-			Destroy (this.gameObject);
+			DontDestroyOnLoad(this.gameObject);
+			DontDestroyOnLoad(IF_name.gameObject.transform.parent.gameObject);
+		}
+		else
+		{
+			Destroy(this.gameObject);
+			Destroy(IF_name.gameObject.transform.parent.gameObject);
 		}
 	}
 	
